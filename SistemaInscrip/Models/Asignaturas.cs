71405881d6f2758883fa52073cc834640006be88
Emmanuel <Emmanuel@DESKTOP-B6P5QMH>
2020-02-23 00:08:11 -0400

    Modelos

---
+++ SistemaInscrip/Models/Asignaturas.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SistemaInscrip.Models
+{
+    public class Asignaturas
+    {
+        
+
+        [Key]
+        public int AsignaturaId { get; set; }
+        public int Codigo { get; set; }
+        public string Descripcion { get; set; }
+        public string Prerequisito { get; set; }
+        public int Creditos  { get; set; }
+
+        public Asignaturas()
+        {
+            AsignaturaId = 0;
+            Codigo = 0;
+            Descripcion = string.Empty;
+            Prerequisito = string.Empty;
+            Creditos = 0;
+        }
+    }
+    
+}
---
+++ SistemaInscrip/Models/Estudiantes.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SistemaInscrip.Models
+{
+    public class Estudiantes
+    {
+        [Key]
+        public int EstudianteId { get; set; }
+        public int Matricula { get; set; }
+        public string Nombres { get; set; }
+        public decimal Balance { get; set; }
+
+        public Estudiantes()
+        {
+            EstudianteId = 0;
+            Matricula = 0;
+            Nombres = string.Empty;
+            Balance = 0;
+        }
+    }
+}
